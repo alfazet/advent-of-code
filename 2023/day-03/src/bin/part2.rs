3c47a002edc2f022ccd017df1973754da875b2b1
azasada <zsd.antoni@gmail.com>
2023-12-03 21:09:57 +0100

    added 3/1

--- 2023/day-03/src/bin/part2.rs
+++
@@ -1,7 +0,0 @@
-use day_03::part2::solve;
-
-fn main() {
-    let input = include_str!("../../input.in");
-    let ans = solve(input);
-    println!("{}", ans);
-}
--- 2023/day-03/src/lib.rs
+++ 2023/day-03/src/lib.rs
@@ -1,3 +1,2 @@
 // Advent of Code 2023, Day 3: Gear Ratios
 pub mod part1;
-pub mod part2;
